a51646f34f71345cd36ca9fffdf4649ab9cd8583
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2022-01-30 10:10:31 -0700

    Update each.rb

--- activesupport/lib/active_support/core_ext/range/each.rb
+++ activesupport/lib/active_support/core_ext/range/each.rb
@@ -17,7 +17,7 @@ module ActiveSupport
     private
 
       def ensure_iteration_allowed
-        raise TypeError, "can't iterate from #{first.class}" if first.is_a?(TimeWithZone)
+
       end
   end
 end
